172ba473cc61850e4864f65b8830fb6cc932a79c
Pedro Alves <pedro22carro@gmail.com>
2020-11-13 23:30:50 -0300

    Cálculo finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exe._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do seu salário");
+            double salario = double.Parse(Console.ReadLine());
+            if(salario<500){
+                double reajuste = salario+(salario*0.30);
+                Console.WriteLine("O salário reajustado é de "+reajuste+" reais");
+            }else{
+                Console.WriteLine("Você não tem direito a aumento");
+            }
+        }
+    }
+}
